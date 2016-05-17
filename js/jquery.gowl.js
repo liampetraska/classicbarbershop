261c1983bd1a2dd7ead1938f14477b9bb651e347
Liam Petraska <liam.petraska@icloud.com>
2016-05-17 10:53:10 -0400

    First Push
    Bringing project into GitHub

---
+++ js/jquery.gowl.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	
+	$('nav ul li a').click(function(){
+	
+		var el = $(this).attr('href');
+		var elWrapped = $(el);
+		
+		scrollToDiv(elWrapped,140);
+		
+		return false;
+	
+	});
+	
+	function scrollToDiv(element,navheight){
+	
+		
+	
+		var offset = element.offset();
+		var offsetTop = offset.top;
+		var totalScroll = offsetTop-navheight;
+		
+		$('body,html').animate({
+				scrollTop: totalScroll
+		}, 500);
+	
+	}
+	
+	
+	
+});
